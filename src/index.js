60b20d3a6110693c7c488630778b9e79ccb1d8ef
nilchaos87 <justthebit@inorbit.com>
2017-06-05 12:04:15 -0700

    Restored missing Panel import

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import Button from './Button';
 import IconButton from './IconButton';
 import Menu from './Menu';
 import MenuItem from './MenuItem';
+import Panel from './Panel';
 import Select from './Select';
 import Shell from './Shell';
 import Textbox from './Textbox';
